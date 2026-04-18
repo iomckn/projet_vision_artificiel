4f9254e515c078a4a4d70dba4854f3d0258647cf
iomckn <156865257+iomckn@users.noreply.github.com>
2026-04-18 16:30:50 -0400

    Add image and video processing for model predictions

--- app.py
+++ app.py
@@ -50,12 +50,57 @@ def upload():
     input_path = os.path.join("uploads", file.filename)
     file.save(input_path)
 
+    filename = file.filename.lower()
+
+    is_image = filename.endswith((".jpg", ".jpeg", ".png"))
+    is_video = filename.endswith((".mp4", ".avi", ".mov"))
+
     model_path = os.path.join("models", model_name)
 
     if model_name == "model_RTDETR.pt":
         model = RTDETR(model_path)
     else:
         model = YOLO(model_path)
+
+    if is_image:
+        results = model(input_path)[0]
+
+        annotated = results.plot()
+
+        output_path = os.path.join("static", "output.jpg")
+        cv2.imwrite(output_path, annotated)
+
+        boxes = results.boxes
+
+        max_players = 0
+        ball_detected = False
+        confidences = []
+
+        if boxes is not None:
+            for box in boxes:
+                cls = int(box.cls[0])
+                conf = float(box.conf[0])
+
+                confidences.append(conf)
+                label = model.names[cls]
+
+                if label == "human":
+                    max_players += 1
+
+                if label == "ball":
+                    ball_detected = True
+
+        avg_conf = (sum(confidences) / len(confidences) * 100) if confidences else 0
+
+        return render_template(
+            "result.html",
+            file_type="image",
+            file_name="output.jpg",
+            players=max_players,
+            confidence=round(avg_conf, 1),
+            ball="Oui" if ball_detected else "Non",
+            model_name=model_name
+        )
 
     # ouvrir vidéo
     cap = cv2.VideoCapture(input_path)
@@ -73,17 +118,27 @@ def upload():
     ball_detected = False
     confidences = []
 
+    model.predictor = None 
+    all_track_ids = set()
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        results = model(frame)[0]
+        if model_name == "model_RTDETR.pt":
+            results = model(frame)[0]
+        else: 
+            results = model.track(
+                frame,
+                tracker="bytetrack.yaml",
+                persist=True,
+                conf=0.7,
+                verbose=False)[0]
 
         annotated_frame = results.plot()
         out.write(annotated_frame)
 
-        # 🔥 récupérer les detections
+        # récupérer les detections
         boxes = results.boxes
 
         if boxes is not None:
@@ -96,12 +151,13 @@ def upload():
                 label = model.names[cls]
 
                 if label == "human":
-                    current_players += 1
+                    if box.id is not None:
+                        all_track_ids.add(int(box.id[0]))
 
                 if label == "ball":
                     ball_detected = True
 
-            max_players = max(max_players, current_players)
+    max_players = len(all_track_ids)
 
     # moyenne confiance
     if len(confidences) > 0:
@@ -117,6 +173,8 @@ def upload():
 
     return render_template(
         "result.html",
+        file_name="output.mp4",
+        file_type="video",
         video_name=video_name,
         players=max_players,
         confidence=round(avg_conf, 1),
